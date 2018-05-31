225420bd5ab1ae7cb6f0b1912c19057be8641927
David Huard <david.huard@gmail.com>
2018-05-31 16:34:05 -0400

    fixed doc building errors

--- {{cookiecutter.project_slug}}/docs/source/conf.py
+++ {{cookiecutter.project_slug}}/docs/source/conf.py
@@ -34,6 +34,7 @@ import {{ cookiecutter.project_slug }}
 extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.viewcode',
               'sphinx.ext.napoleon',
+              'sphinx.ext.todo',
               #'pywps.ext_autodoc', # Available on master branch. Will be distributed with the 4.2 release.
               ]
 
