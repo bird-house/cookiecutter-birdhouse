audreyfeldroy/cookiecutter-pypackage

Fix #14: updated to next-generation deployment with ansible

{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/cli.py
@@ -6,22 +6,33 @@
 ###########################################################
 
 import os
-
+import click
+from jinja2 import Environment, PackageLoader, select_autoescape
 from pywps import configuration
 
 from . import wsgi
 from six.moves.urllib.parse import urlparse
 
-import logging
-logging.basicConfig(format='%(message)s', level=logging.INFO)
-LOGGER = logging.getLogger('DEMO')
+template_env = Environment(
+    loader=PackageLoader('{{ cookiecutter.project_slug }}', 'templates'),
+    autoescape=select_autoescape(['yml', 'xml'])
+)
+
+
+def write_user_config(**kwargs):
+    config_templ = template_env.get_template('pywps.cfg')
+    rendered_config = config_templ.render(**kwargs)
+    config_file = os.path.abspath(os.path.join(os.path.curdir, "custom.cfg"))
+    with open(config_file, 'w') as fp:
+        fp.write(rendered_config)
+    return config_file
 
 
 def get_host():
     url = configuration.get_config_value('server', 'url')
-    url = url or 'http://localhost:5000/wps'
+    url = url or 'http://localhost:{{ cookiecutter.http_port }}/wps'
 
-    LOGGER.warn("starting WPS service on %s", url)
+    click.echo("starting WPS service on {}".format(url))
 
     parsed_url = urlparse(url)
     if ':' in parsed_url.netloc:
@@ -46,56 +57,66 @@ def _run(application, bind_host=None, daemon=False):
         hostname=bind_host,
         port=port,
         application=application,
-        use_debugger=True,
-        use_reloader=True,
+        use_debugger=False,
+        use_reloader=False,
+        threaded=True,
+        # processes=2,
         use_evalex=not daemon,
         static_files=static_files)
 
 
-def main():
-    import argparse
+@click.command()
+@click.option('--config', metavar='PATH', help='path to pywps configuration file.')
+@click.option('--bind-host', metavar='IP-ADDRESS', default='0.0.0.0',
+              help='IP address used to bind service.')
+@click.option('--daemon/--no-daemon', default=False, help='run in daemon mode.')
+@click.option('--hostname', metavar='HOSTNAME', default='localhost', help='hostname in PyWPS configuration.')
+@click.option('--port', metavar='PORT', default='{{ cookiecutter.http_port }}', help='port in PyWPS configuration.')
+@click.option('--maxsingleinputsize', default='200mb', help='maxsingleinputsize in PyWPS configuration.')
+@click.option('--maxprocesses', metavar='INT', default='10', help='maxprocesses in PyWPS configuration.')
+@click.option('--parallelprocesses', metavar='INT', default='2', help='parallelprocesses in PyWPS configuration.')
+@click.option('--log-level', metavar='LEVEL', default='INFO', help='log level in PyWPS configuration.')
+@click.option('--log-file', metavar='PATH', default='pywps.log', help='log file in PyWPS configuration.')
+@click.option('--database', default='sqlite:///pywps-logs.sqlite', help='database in PyWPS configuration')
+def cli(config, bind_host, daemon, hostname, port,
+        maxsingleinputsize, maxprocesses, parallelprocesses,
+        log_level, log_file, database):
+    """Command line for starting a PyWPS service.
+    This service is by default available at http://localhost:{{ cookiecutter.http_port }}/wps
 
-    parser = argparse.ArgumentParser(
-        description="""Script for starting a demo WPS instance.
-                       This service is by default available at http://localhost:5000/wps""",
-        epilog="""Do not use this service in a production environment.
-         It's intended to be running in a test environment only!
-         For more documentation, visit http://bird-house.github.io/
-        """
-    )
-    parser.add_argument('--debug',
-                        action="store_true", help="enable debug logging mode")
-    parser.add_argument('-c', '--config',
-                        help="path to pywps configuration file")
-    parser.add_argument('-a', '--all-addresses',
-                        action='store_true', help="run service using IPv4 0.0.0.0 (all network interfaces), "
-                        "otherwise bind to 127.0.0.1 (localhost).")
-    parser.add_argument('-d', '--daemon',
-                        action='store_true', help="run in daemon mode")
-    args = parser.parse_args()
+    Do not use this service in a production environment.
+    It's intended to be running in a test environment only!
+    For more documentation, visit http://pywps.org/doc
+    """
     cfgfiles = []
-    if args.config:
-        cfgfiles.append(args.config)
-        LOGGER.warn('using pywps configuration: %s', args.config)
-    if args.debug:
-        cfgfiles.append(os.path.join(os.path.dirname(__file__), 'debug.cfg'))
-    if args.all_addresses:
-        bind_host = '0.0.0.0'
-    else:
-        bind_host = '127.0.0.1'
+    cfgfiles.append(write_user_config(
+        wps_hostname=hostname,
+        wps_port=port,
+        wps_maxsingleinputsize=maxsingleinputsize,
+        wps_maxprocesses=maxprocesses,
+        wps_parallelprocesses=parallelprocesses,
+        wps_log_level=log_level,
+        wps_log_file=log_file,
+        wps_database=database,
+    ))
+    if config:
+        cfgfiles.append(config)
     app = wsgi.create_app(cfgfiles)
     # let's start the service ...
-    if args.daemon:
+    # See:
+    # * https://github.com/geopython/pywps-flask/blob/master/demo.py
+    # * http://werkzeug.pocoo.org/docs/0.14/serving/
+    if daemon:
         # daemon (fork) mode
         pid = None
         try:
             pid = os.fork()
             if pid:
-                LOGGER.warn('forked process id: %s', pid)
+                click.echo('forked process id: {}'.format(pid))
         except OSError as e:
             raise Exception("%s [%d]" % (e.strerror, e.errno))
 
-        if (pid == 0):
+        if pid == 0:
             os.setsid()
             _run(app, bind_host=bind_host, daemon=True)
         else:
@@ -103,7 +124,3 @@ def main():
     else:
         # no daemon
         _run(app, bind_host=bind_host)
-
-
-if __name__ == '__main__':
-    main()

bird-house/cookiecutter-birdhouse

{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/cli.py
@@ -1,5 +1,11 @@
 """Console script for {{cookiecutter.project_slug}}."""
 
+###########################################################
+# Demo WPS service for testing and debugging.
+#
+# See the werkzeug documentation on how to use the debugger:
+# http://werkzeug.pocoo.org/docs/0.12/debug/
+###########################################################
 
 import os
 import sys
@@ -9,6 +15,7 @@ import argparse
 {%- if cookiecutter.command_line_interface|lower == 'click' %}
 import click
 {%- endif %}
+from jinja2 import Environment, PackageLoader, select_autoescape
 from pywps import configuration
 
 from . import wsgi
@@ -18,12 +25,24 @@ import logging
 logging.basicConfig(format='%(message)s', level=logging.INFO)
 LOGGER = logging.getLogger('DEMO')
 
+template_env = Environment(
+    loader=PackageLoader('{{ cookiecutter.project_slug }}', 'templates'),
+    autoescape=select_autoescape(['yml', 'xml'])
+)
+
+def write_user_config(**kwargs):
+    config_templ = template_env.get_template('pywps.cfg')
+    rendered_config = config_templ.render(**kwargs)
+    config_file = os.path.abspath(os.path.join(os.path.curdir, "custom.cfg"))
+    with open(config_file, 'w') as fp:
+        fp.write(rendered_config)
+    return config_file
 
 def get_host():
     url = configuration.get_config_value('server', 'url')
-    url = url or 'http://localhost:5000/wps'
+    url = url or 'http://localhost:{{ cookiecutter.http_port }}/wps'
 
-    LOGGER.warn("starting WPS service on %s", url)
+    LOGGER.warn("starting WPS service on %s" % url)
 
     parsed_url = urlparse(url)
     if ':' in parsed_url.netloc:
@@ -48,63 +67,66 @@ def _run(application, bind_host=None, daemon=False):
         hostname=bind_host,
         port=port,
         application=application,
-        use_debugger=True,
-        use_reloader=True,
+        use_debugger=False,
+        use_reloader=False,
+        threaded=True,
+        # processes=2,
         use_evalex=not daemon,
         static_files=static_files)
 
 {% if cookiecutter.command_line_interface|lower == 'click' %}
 @click.command()
-def main(args=None):
-    """Console script for {{cookiecutter.project_slug}}."""
-    click.echo("Replace this message by putting your code into "
-               "{{cookiecutter.project_slug}}.cli.main")
-    click.echo("See click documentation at https://click.palletsprojects.com/")
-    return 0
-{%- endif %}
-{%- if cookiecutter.command_line_interface|lower == 'argparse' %}
-def main():
-    parser = argparse.ArgumentParser(
-        description="""Script for starting a demo WPS instance.
-                       This service is by default available at http://localhost:5000/wps""",
-        epilog="""Do not use this service in a production environment.
-         It's intended to be running in a test environment only!
-         For more documentation, visit http://bird-house.github.io/
-        """
-    )
-    parser.add_argument('--debug',
-                        action="store_true", help="enable debug logging mode")
-    parser.add_argument('-c', '--config',
-                        help="path to pywps configuration file")
-    parser.add_argument('-a', '--all-addresses',
-                        action='store_true', help="run service using IPv4 0.0.0.0 (all network interfaces), "
-                        "otherwise bind to 127.0.0.1 (localhost).")
-    parser.add_argument('-d', '--daemon',
-                        action='store_true', help="run in daemon mode")
-    args = parser.parse_args()
+@click.option('--config', metavar='PATH', help='path to pywps configuration file.')
+@click.option('--bind-host', metavar='IP-ADDRESS', default='0.0.0.0',
+              help='IP address used to bind service.')
+@click.option('--daemon/--no-daemon', default=False, help='run in daemon mode.')
+@click.option('--hostname', metavar='HOSTNAME', default='localhost', help='hostname in PyWPS configuration.')
+@click.option('--port', metavar='PORT', default='{{ cookiecutter.http_port }}', help='port in PyWPS configuration.')
+@click.option('--maxsingleinputsize', default='200mb', help='maxsingleinputsize in PyWPS configuration.')
+@click.option('--maxprocesses', metavar='INT', default='10', help='maxprocesses in PyWPS configuration.')
+@click.option('--parallelprocesses', metavar='INT', default='2', help='parallelprocesses in PyWPS configuration.')
+@click.option('--log-level', metavar='LEVEL', default='INFO', help='log level in PyWPS configuration.')
+@click.option('--log-file', metavar='PATH', default='pywps.log', help='log file in PyWPS configuration.')
+@click.option('--database', default='sqlite:///pywps-logs.sqlite', help='database in PyWPS configuration')
+def cli(config, bind_host, daemon, hostname, port,
+        maxsingleinputsize, maxprocesses, parallelprocesses,
+        log_level, log_file, database):
+    """Command line for starting a PyWPS service.
+    This service is by default available at http://localhost:{{ cookiecutter.http_port }}/wps
+
+    Do not use this service in a production environment.
+    It's intended to be running in a test environment only!
+    For more documentation, visit http://pywps.org/doc
+    """
     cfgfiles = []
-    if args.config:
-        cfgfiles.append(args.config)
-        LOGGER.warn('using pywps configuration: %s', args.config)
-    if args.debug:
-        cfgfiles.append(os.path.join(os.path.dirname(__file__), 'debug.cfg'))
-    if args.all_addresses:
-        bind_host = '0.0.0.0'
-    else:
-        bind_host = '127.0.0.1'
+    cfgfiles.append(write_user_config(
+        wps_hostname=hostname,
+        wps_port=port,
+        wps_maxsingleinputsize=maxsingleinputsize,
+        wps_maxprocesses=maxprocesses,
+        wps_parallelprocesses=parallelprocesses,
+        wps_log_level=log_level,
+        wps_log_file=log_file,
+        wps_database=database,
+    ))
+    if config:
+        cfgfiles.append(config)
     app = wsgi.create_app(cfgfiles)
     # let's start the service ...
-    if args.daemon:
+    # See:
+    # * https://github.com/geopython/pywps-flask/blob/master/demo.py
+    # * http://werkzeug.pocoo.org/docs/0.14/serving/
+    if daemon:
         # daemon (fork) mode
         pid = None
         try:
             pid = os.fork()
             if pid:
-                LOGGER.warn('forked process id: %s', pid)
+                LOGGER.warn('forked process id: {}'.format(pid))
         except OSError as e:
             raise Exception("%s [%d]" % (e.strerror, e.errno))
 
-        if (pid == 0):
+        if pid == 0:
             os.setsid()
             _run(app, bind_host=bind_host, daemon=True)
         else:
@@ -113,7 +135,3 @@ def main():
         # no daemon
         _run(app, bind_host=bind_host)
 {%- endif %}
-
-
-if __name__ == "__main__":
-    sys.exit(main())  # pragma: no cover