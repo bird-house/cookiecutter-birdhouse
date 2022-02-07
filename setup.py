a5d7e28ce0d66afc3ae73c483d5457e4ac808743
MacPingu <cehbrecht@users.noreply.github.com>
2022-02-07 19:29:42 +0100

    Update to pywps 4.5 (#112)
    * update cruft
    
    * skip py36, added py39
    
    * update pywps
    
    * added github ci
    
    * removed travis
    
    * leverage make and tox in GitHub actions
    
    * install tox
    
    * adjust tox builds
    
    * skip gdal-based tests on PyPy
    
    Co-authored-by: Trevor James Smith <10819524+Zeitsperre@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 # !/usr/bin/env python
 
 from distutils.core import setup
+
 setup(
     name='cookiecutter-pypackage',
     packages=[],
     version='0.5.0',
     description='Cookiecutter template for a PyWPS service',
     license='BSD',
-    author='Birdhouse',
-    author_email='wps@dkrz.de',
-    url='https://github.com/audreyr/cookiecutter-pypackage',
+    author='Carsten Ehbrecht',
+    author_email='ehbrecht@dkrz.de',
+    url='https://github.com/bird-house/cookiecutter-birdhouse',
     keywords=['cookiecutter', 'template', 'package', 'wps' 'pywps', 'birdhouse'],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
@@ -20,9 +21,9 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development',
