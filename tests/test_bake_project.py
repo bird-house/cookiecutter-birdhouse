33e8079a89cf54c0291309afab05d689eef00785
Trevor James Smith <10819524+Zeitsperre@users.noreply.github.com>
2024-09-16 15:52:17 -0400

    remove setup.cfg

--- tests/test_bake_project.py
+++ tests/test_bake_project.py
@@ -79,7 +79,6 @@ def test_bake_with_defaults(cookies):
 
         found_toplevel_files = [f.basename for f in result.project.listdir()]
         assert 'setup.py' in found_toplevel_files
-        assert 'setup.cfg' in found_toplevel_files
         assert 'tox.ini' in found_toplevel_files
         assert 'tests' in found_toplevel_files
 
